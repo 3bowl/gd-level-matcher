08c88be764165fe09926eda92f813d6250f1124a
3bowl <branforsaken@gmail.com>
2026-08-17 18:50:02 -0400

    Partial match support added

--- README.md
+++ README.md
@@ -30,9 +30,10 @@
 2. The program will ask that you supply a comparison level ID. This level ID will act as your fingerprint. The IDs that you're about to scan will attempt to match up with this very comparison level's object data, one object at a time.
 3. Next, the program will have you supply a starting level ID. This will be the lower bound for the level ID range scan that you're about to do.
 4. Lastly, the program will have you supply the amout of levels IDs to scrape through.
-5. Let the magic happen! Every ID it comes by is logged to `LOGS/`, and every found match will be logged to `LOGS/FOUND/`.
-6. If Cloudflare intercepts, the execution ends and the error log will be written to `LOGS/`.
-7. If you wish to start fresh on a subsequent run, feel free to delete the log folders and/or the `comp_cache` folder.
+5. Let the magic happen! Every ID it comes by is logged to `LOGS/`.
+6. If the comparison level matches up perfectly with another level, it will be a full match, and will be written to `LOGS/FOUND/`. But if the comparison only partially matches up with another level, it will be a partial match, and will be written to `LOGS/PARTIAL_FOUND/`.
+7. If Cloudflare intercepts, the execution ends and the error log will be written to `LOGS/`.
+8. If you wish to start fresh on a subsequent run, feel free to delete the log folders and/or the `comp_cache` folder.
 
 ## How to use the comparison level effectively
 While you could technically attempt a match-up scan using an entire level as the comparison, it's better to instead make a copy of the level yourself and manually strip it down to a very small sample of objects unique to the level. That way, if the program encounters a copy level, it will be less susceptible to any modifications done by the copier.
@@ -44,7 +45,8 @@ Once you make the stripped-down fingerprint of the level you're scanning for, up
 ## Notes and precautions
 ### Here are some things to be aware of when using the program:
 * **"I uploaded a perfect copy of a level as a comparison, but when I try to match it up with the original, it won't detect it. Why?"** Note that some object IDs (including some older ones) have been remapped to different object IDs in later updates. When you save an old level in a modern version, the outdated IDs will remap to the new ones. This can and will affect the matching process, giving false negatives. You have to be careful with the objects present in your fingerprint--that is, only include objects whose IDs never changed since the original level's creation.
-* **Please respect RobTop's servers.** Please do not nullify the request cooldown, as that is there for safety purposes. It may seem inconvenient, but if you want to go faster, you'll need multiple devices connected to multiple separate IP addresses. You are responsible for any damage done to the servers when you use this program.
+* **Please respect RobTop's servers.** Please do not nullify/mitigate the request cooldown, as that is there for safety purposes. It may seem inconvenient, but if you want to go faster, you'll need multiple devices connected to multiple separate IP addresses. You are responsible for any damage done to the servers when you use this program.
+* **There exists a cutoff for partial matches and non-matches.** If at least 50% of the comparison level matches up with the other level, it will be considered a partial match, and will be written to `LOGS/PARTIAL_FOUND/`. But if it's under 50%, it gets treated as an ordinary non-matching level.
 * **Patience is key!** This program will time to execute. Assuming that each request takes 5 seconds, `1000` IDs will take ~1.4 hours. Again, if you wish to go faster, you'll need to utilize separate devices running on separate IPs.
 
 #
--- src/Match.py
+++ src/Match.py
@@ -28,25 +28,42 @@ def get_listed_obj_data_wrapper(id: int):
     return lvl_name, obj_list
 
 
-def write_log(id: int, lvl_name: str, exists: bool, object_string=""):
-    """Writes a log from the level ID, level name, an existence boolean, and, if it's a found matching level, the object string.\n
-    If the level is a found match (object_string fed in), the logs are written to /LOGS/FOUND\n
-    If the level hasn't been matched yet (object_string not fed in), the log is written to /LOGS"""
-    if object_string:   # write both the info and the object data to a unique folder
-        dir = os.path.join(ROOT, f"LOGS/FOUND/{id}/")
-        os.makedirs(dir, exist_ok=True)
+def write_log(id: int, lvl_name: str, exists=True, object_string="", partial_match=False, percentage=100.0):
+    """Writes a log from the level ID, level name, an existence boolean (True by default), and, if it's a fully matching level, the object string.\n
+    If the level is a full match (object_string fed in), the logs are written to /LOGS/FOUND\n
+    If the level hasn't been matched yet (object_string not fed in), the log is written to /LOGS\n
+    If the level is a partial match (object_string not fed in), the log is written to /LOGS/PARTIAL_FOUND\n
+    partial_match: normally set to False, but update to true if you wish to include match percentage in the partial find's log\n
+    percentage: the percentage value of the partial match that should be sent in"""
 
-        with open(os.path.join(dir, "Info.txt"), "w") as info:
+    if object_string:
+        # Full match
+        # write both the info and the object data to a unique folder
+        directory = os.path.join(ROOT, f"LOGS/FOUND/{id}/")
+        os.makedirs(directory, exist_ok=True)
+
+        with open(os.path.join(directory, "Info.txt"), "w") as info:
             timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            info.write(f"{id} | {lvl_name} | Real: {exists}            [ Logged: {timestamp} ]\n")
+            info.write(f"{id} | {lvl_name} | Match percentage: 100%            [ Logged: {timestamp} ]\n")
 
-        with open(os.path.join(dir, "Object_data.txt"), "w") as objs:
+        with open(os.path.join(directory, "Object_data.txt"), "w") as objs:
             objs.write(object_string)
-    else:   # write the info to the general log
-        dir = os.path.join(ROOT, "LOGS/")
-        os.makedirs(dir, exist_ok=True)
 
-        with open(os.path.join(dir, "ID_log.txt"), "a+") as log:
+    elif partial_match:
+        # Partial match
+        # write only the info to /LOGS/PARTIAL_FOUND/Partial_match_log.txt
+        directory = os.path.join(ROOT, "LOGS/PARTIAL_FOUND/")
+        os.makedirs(directory, exist_ok=True)
+
+        with open(os.path.join(directory, "Partial_match_log.txt"), "a") as partial:
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            partial.write(f"{id} | {lvl_name} | Match percentage: {percentage}%            [ Logged: {timestamp} ]\n")
+
+    else:   # write the info to the general log
+        directory = os.path.join(ROOT, "LOGS/")
+        os.makedirs(directory, exist_ok=True)
+
+        with open(os.path.join(directory, "ID_log.txt"), "a") as log:
             timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             log.write(f"{id} | {lvl_name} | Real: {exists}            [ Logged: {timestamp} ]\n")
 
@@ -56,6 +73,8 @@ def compare(comp_obj_list: list, floor: int, amount: int):
     comp_obj_list: the list of objects that will be compared with\n
     floor: the lower bound of the ID scan\n
     amount: the amount of IDs to scan"""
+
+    # Here's the master loop that handles ID ranges for matching
     ceiling = floor + amount
     for id in range(floor, ceiling):
         # nullify these, since they carry over from the previous iteration
@@ -68,29 +87,52 @@ def compare(comp_obj_list: list, floor: int, amount: int):
             lvl_name = data[0]  # current level's name
             obj_str = data[1]  # current level's object data (in raw string form)
 
-            write_log(id, lvl_name, exists=True)
+            write_log(id, lvl_name, exists=True)    # exists is set to either True/False, since we haven't matched yet
         else:
             write_log(id, lvl_name, exists=False)
             continue    # continue if nonexistent
 
-        # The object data comparisons take place here:
+        # THE OBJECT DATA COMPARISONS TAKE PLACE HERE #
+        matched_objs = 0    # track the count of matched objects
         for comp_object in comp_obj_list:
-            if comp_object not in obj_str:  # obj_str = current level's object string
-                break
-        else:
-            # This block runs only if the loop finished without hitting 'break'
-            print("***Match found! Saving to LOGS > FOUND...***")
-            play(NOTIFICATION_PATH)
+            if comp_object in obj_str:  # obj_str = current level's object string
+                matched_objs += 1
+        ###############################################
 
-            write_log(id, lvl_name, exists=True, object_string=obj_str)
+        # After scanning the entire ID...
+        if matched_objs != 0:
+            # If execution reaches here, there was at least one object that matched up
+            if matched_objs >= len(comp_obj_list):
+                
+                # This is a full match
+                print(f"***Full match found! Saving to LOGS > FOUND > {id}...***")
+                play(FULL_NOTIF_PATH)
+
+                write_log(id, lvl_name, object_string=obj_str)
+            else:
+                match_percent = (matched_objs / len(comp_obj_list)) * 100
+                if match_percent >= MIN_MATCH_THRESHOLD:    # We must exceed/equal the threshold first to write the partial find to disk
+
+                    # This is a partial match
+                    print("*Partial match found. Saving to LOGS > PARTIAL_FOUND > Partial_match_log.txt...*")
+                    play(PARTIAL_NOTIF_PATH)
+
+                    write_log(id, lvl_name, partial_match=True, percentage=f"{match_percent:.2f}")
+        
+        # If no object matched, go to the next iteration without doing anything else
 
     print("Search finished.")
+    time.sleep(5)
     return
 
 
 if __name__ == "__main__":
     ROOT = get_project_root()
-    NOTIFICATION_PATH = os.path.join(ROOT, "notif_sounds/Full_match.ogg")
+    FULL_NOTIF_PATH = os.path.join(ROOT, "notif_sounds/Full_match.ogg")
+    PARTIAL_NOTIF_PATH = os.path.join(ROOT, "notif_sounds/Partial_match.ogg")
+
+    # The match-percentage threshold that must be exceeded for a level to be considered a partial match
+    MIN_MATCH_THRESHOLD = 50.0
     
     try:
         comparison_obj_id = int(input("Provide the comparison level ID here >>> "))
@@ -101,22 +143,25 @@ if __name__ == "__main__":
         print("\n--- Use CTRL+C to STOP scanning ---")
         print("\nGetting object data from comparison level ID...")
 
-        # Access the cached comparison level download if available
+        # Access the cached comparison level download if available, otherwise make one for next time
         cache_dir = os.path.join(ROOT, "comp_cache/")
         os.makedirs(cache_dir, exist_ok=True)
 
         cache_path = os.path.join(cache_dir, f"comp_{comparison_obj_id}_cache.json")
-        if not os.path.exists(cache_path):  # if it's not in cache
-            try:
-                comparison_obj_list = get_listed_obj_data_wrapper(comparison_obj_id)[1] # We want to convert the comparison data to a list
+        if not os.path.exists(cache_path):  # if the comparison level's filename not in cache
+
+            # We want to convert the comparison data to a list
+            comparison_obj_data = get_listed_obj_data_wrapper(comparison_obj_id)
+            if comparison_obj_data: # make sure it's not NoneType (nonexistent level) before subscripting
+                comparison_obj_list = comparison_obj_data[1]
                 # Save list as a JSON file in cache
                 with open(cache_path, "w") as cache:
                     json.dump(comparison_obj_list, cache)
-            except TypeError:   # The comparison level is nonexistent at this point. You can't subscript NoneType
+            else:
                 print("Comparison level ID is nonexistent. Closing the program...")
-
                 time.sleep(5)
                 sys.exit(1)
+
         else:   # if it does exist in cache
             # Load list back from the JSON file
             print("Loading comparison level data from local cache...")
